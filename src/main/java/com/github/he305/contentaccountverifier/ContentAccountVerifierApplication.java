3ce65bb6ced3184ac01ab657016ffab776460553
sergey <he305@mail.ru>
2022-08-25 14:07:35 +0300

    Initial commit

---
+++ src/main/java/com/github/he305/contentaccountverifier/ContentAccountVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.github.he305.contentaccountverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentAccountVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentAccountVerifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/he305/contentaccountverifier/ContentAccountVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.he305.contentaccountverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentAccountVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
