99b067111b612cb3db5d3d86a21c8aeb64931cff
sergey <he305@mail.ru>
2022-08-25 14:32:22 +0300

    CON-57 Eureka client init

--- src/main/java/com/github/he305/contentaccountverifier/ContentAccountVerifierApplication.java
+++ src/main/java/com/github/he305/contentaccountverifier/ContentAccountVerifierApplication.java
@@ -2,8 +2,10 @@ package com.github.he305.contentaccountverifier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ContentAccountVerifierApplication {
 
 	public static void main(String[] args) {
