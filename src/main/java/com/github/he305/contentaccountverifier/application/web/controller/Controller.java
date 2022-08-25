db8007a0d4323f3ac02f03545f3ebd500648ef70
Sergey <he305@mail.ru>
2022-08-25 23:27:10 +0300

    get -> post request (#12) #patch

--- src/main/java/com/github/he305/contentaccountverifier/application/web/controller/Controller.java
+++ src/main/java/com/github/he305/contentaccountverifier/application/web/controller/Controller.java
@@ -3,7 +3,7 @@ package com.github.he305.contentaccountverifier.application.web.controller;
 import com.github.he305.contentaccountverifier.application.web.dto.ContentAccountDto;
 import com.github.he305.contentaccountverifier.domain.service.ContentAccountService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +15,7 @@ public class Controller {
 
     private final ContentAccountService contentAccountService;
 
-    @GetMapping(value = "/verify")
+    @PostMapping(value = "/verify")
     public void verify(@RequestBody ContentAccountDto dto) {
         contentAccountService.verify(dto.getContentAccountName(), dto.getPlatform());
     }
